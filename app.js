2a733699cc4c1678b12abd026c9425f1e04188bf
diegovieiradv <dvdiego87@gmail.com>
2025-11-19 19:15:14 -0300

    adicionei o math.random ao project

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 alert("Olá, bem-vindo ao jogo Number-Secret!");
-
-let numberSecret = 9;
+let numeroMaximo = 5000;
+let numberSecret = parseInt(Math.random() * numeroMaximo + 1); // número entre 1 e 20;
 let chute;
 let tentativas = 1;
 
 while (chute != numberSecret) {
-  chute = prompt("Digite um número entre 1 e 20:");
+  chute = prompt(`Digite um número entre 1 e ${numeroMaximo}:`);
 
   if (chute == numberSecret) {
     break;
@@ -14,7 +14,7 @@ while (chute != numberSecret) {
   } else if (chute > numberSecret) {
     alert("Seu chute é muito alto. Tente novamente!");
   } else {
-    alert("Entrada inválida. Por favor, insira um número entre 1 e 20.");
+    alert(`Entrada inválida. Por favor, insira um número entre 1 e ${numeroMaximo} .`);
   }
   tentativas++;
 }
