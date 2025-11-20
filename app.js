56a9b086083be9a4698a23dbb98a7f1ac84b90af
diegovieiradv <dvdiego87@gmail.com>
2025-11-20 01:10:01 -0300

    adicionei o css do project

--- app.js
+++ app.js
@@ -1,33 +1,5 @@
-alert("Olá, bem-vindo ao jogo Number-Secret!");
-let numeroMaximo = 5000;
-let numberSecret = parseInt(Math.random() * numeroMaximo + 1); // número entre 1 e 20;
-let chute;
-let tentativas = 1;
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "Jogo do número secreto";
 
-while (chute != numberSecret) {
-  chute = prompt(`Digite um número entre 1 e ${numeroMaximo}:`);
-
-  if (chute == numberSecret) {
-    break;
-  } else if (chute < numberSecret) {
-    alert("Seu chute é muito baixo. Tente novamente!");
-  } else if (chute > numberSecret) {
-    alert("Seu chute é muito alto. Tente novamente!");
-  } else {
-    alert(`Entrada inválida. Por favor, insira um número entre 1 e ${numeroMaximo} .`);
-  }
-  tentativas++;
-}
-
-let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
-alert(
-  `Parabéns! Você adivinhou o número secreto é ${numberSecret} com ${tentativas} ${palavraTentativa}.`
-);
-
-/* é o mesmo codigo acima 
-if (tentativas > 1 ) {
-    alert(`Parabéns! Você adivinhou o número secreto é ${numberSecret} com ${tentativas} tentativas.`);
-} else {
-    alert(`Parabéns! Você adivinhou o número secreto é ${numberSecret} com ${tentativas} tentativa.`);
-}
-*/
+let paragrafo = document.querySelector("p");;
+paragrafo.innerHTML = "Descubra o número secreto entre 1 e 100!";
